827c558288d31f986b6e350c269153ebf30b4184
Meas Sombok <82021527+sombokmeas168@users.noreply.github.com>
2024-05-27 09:18:01 +0700

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -1,9 +1,3 @@
-echo ---------------------------------------
-echo downloading Main Activator file.....
-echo ---------------------------------------
-
-
-
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
 $DownloadURL = 'https://raw.githubusercontent.com/sombokmeas168/windows-activ/main/Main%20Activator.cmd'
@@ -24,5 +18,5 @@ if (Test-Path $FilePath) {
 }
 
 echo -----------------------------------------
-echo finished............
+echo Finished.
 echo -----------------------------------------
